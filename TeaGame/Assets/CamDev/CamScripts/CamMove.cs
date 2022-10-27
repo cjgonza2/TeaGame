482bb173790eacc2d85f741d3fef27d80d58e470
cjgonza2 <camrong520@gmail.com>
2022-10-27 14:27:58 -0400

    Added reset Animation, started State Machine for animation transitions.

--- TeaGame/Assets/CamDev/CamScripts/CamMove.cs
+++ TeaGame/Assets/CamDev/CamScripts/CamMove.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 
 public class CamMove : MonoBehaviour
 {
-    
+
+
     //do tween
     public Animator animator;
     
     Vector3 mousePosOffset;
     private float mouseZ;
 
+    public enum State
+    {
+        Default,
+        Pouring,
+        Reset
+    }
     // Start is called before the first frame update
     void Start()
     {
+        TransitionState(State.Default);
+    }
 
+    public void TransitionState(State newstate)
+    {
+        //currentState = newstate
     }
 
     private void OnMouseDown()
@@ -41,18 +53,20 @@ public class CamMove : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Cup"))
+        if (col.gameObject.CompareTag("cup"))
         {
-            animator.SetTrigger("pouring");
+            animator.SetBool("pouring", true);
+            //animator.SetTrigger("pouring");
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D col)
+    public void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Poop");
-        /*if (col.gameObject.CompareTag("Cup"))
+        if (other.gameObject.CompareTag("cup"))
         {
-            animator.SetTrigger("pouring");
-        }*/
+            animator.SetTrigger("reset");
+            animator.SetBool("pouring", false);
+            //animator.SetTrigger("reset");
+        }
     }
 }
