7b9c9e5716c337dac7892f938839126fb8b468c7
cjgonza2 <camrong520@gmail.com>
2023-04-13 14:07:20 -0400

    intro scene plays all the lines of text

--- TeaGame/Assets/CamDev/CamScripts/StoryManager.cs
+++ TeaGame/Assets/CamDev/CamScripts/StoryManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Ink.Runtime;
 using TMPro;
+using Unity.VisualScripting;
 
 public class StoryManager : MonoBehaviour
 {
@@ -20,23 +21,27 @@ public class StoryManager : MonoBehaviour
     [Header("Line Three")]
     [SerializeField] private Animator lineThree_Animator;
     [Header("Line Four")]
-    [SerializeField] private Animator LineFour_Animator;
+    [SerializeField] private Animator lineFour_Animator;
     [Header("Line Five")]
     [SerializeField] private Animator lineFive_Animator;
     [Header("Line Six")]
-    [SerializeField]private Animator lineSix_Animator;
+    [SerializeField] private Animator lineSix_Animator;
     [Header("Line Seven")]
-    [SerializeField]private Animator lineSeven_Animator;
+    [SerializeField] private Animator lineSeven_Animator;
     [Header("Line Eight")]
-    [SerializeField]private Animator lineEight_Animator;
+    [SerializeField] private Animator lineEight_Animator;
     [Header("Line Nine")]
-    [SerializeField]private Animator lineNine_Animator;
+    [SerializeField] private Animator lineNine_Animator;
     [Header("Line Ten")]
-    [SerializeField]private Animator lineTen_Animator;
+    [SerializeField] private Animator lineTen_Animator;
     [Header("Line Eleven")]
-    [SerializeField]private Animator lineEleven_Animator;
+    [SerializeField] private Animator lineEleven_Animator;
     [Header("Line Twelve")]
-    [SerializeField]private Animator lineTwelve_Animator;
+    [SerializeField] private Animator lineTwelve_Animator;
+    [Header("Line Thirteen")]
+    [SerializeField] private Animator lineThirteen_Animator;
+    [Header("Line Fourteen")]
+    [SerializeField] private Animator lineFourteen_Animator;
     [Header("Panel")] 
     [SerializeField] private Animator SceneFade;
 
@@ -54,7 +59,7 @@ public class StoryManager : MonoBehaviour
 
     private IEnumerator InitialLines()
     {
-        for (int i = 0; i < 4; i++) //adds 4 lines to string list.
+        for (int i = 0; i < 14; i++) //adds 13 lines to string list.
         {
             _lines.Add(_inkStory.Continue()); //adds each line of the story
             _text[i].text = _lines[i];           //to string list.
@@ -65,77 +70,127 @@ public class StoryManager : MonoBehaviour
         yield break;
     }
 
-    #region SceneOne
-
-    #region Line One
+    #region SectionOne
+    
     private IEnumerator FadeInLineOne()
     {
         lineOne_Animator.Play("LineOne_fadeIn");
         yield return new WaitForSeconds(4f);
         StartCoroutine(FadeInLineTwo());
     }
-
-    private IEnumerator FadeOutLineOne()
-    {
-        lineOne_Animator.Play("LineOne_FadeOut");
-        yield return new WaitForSeconds(lineOne_Animator.GetCurrentAnimatorClipInfo(0).Length);
-    }
-    #endregion
-
     private IEnumerator FadeInLineTwo()
     {
         lineTwo_Animator.Play("LineTwo_FadeIn");
         yield return new WaitForSeconds(2f);
         StartCoroutine(FadeInLineThree());
     }
-
-    private IEnumerator FadeOutLineTwo()
-    {
-        lineTwo_Animator.Play("LineTwo_FadeOut");
-        yield return new WaitForSeconds(1f);
-        StartCoroutine(FadeOutLineOne());
-    }
-
     private IEnumerator FadeInLineThree()
     {
         lineThree_Animator.Play("LineThree_FadeIn");
         yield return new WaitForSeconds(3f);
         StartCoroutine(FadeInLineFour());
     }
-
-    private IEnumerator FadeOutLineThree()
-    {
-        lineThree_Animator.Play("LineThree_FadeOut");
-        yield return new WaitForSeconds(1f);
-        StartCoroutine(FadeOutLineTwo());
-        yield break;
-    }
-
     private IEnumerator FadeInLineFour()
     {
-        LineFour_Animator.Play("LineFour_FadeIn");
+        lineFour_Animator.Play("LineFour_FadeIn");
         yield return new WaitForSeconds(5f);
-        StartCoroutine(FadeInLineFive());
+        StartCoroutine(BatchFadeOutOne());
     }
-
-    private IEnumerator FadeOutLineFour()
+    private IEnumerator BatchFadeOutOne()
     {
-        LineFour_Animator.Play("LineFour_FadeOut");
+        lineFour_Animator.Play("LineFour_FadeOut");
+        lineThree_Animator.Play("LineThree_FadeOut");
+        lineTwo_Animator.Play("LineTwo_FadeOut");
+        lineOne_Animator.Play("LineOne_FadeOut");
         yield return new WaitForSeconds(1f);
-        StartCoroutine(FadeOutLineThree());
-    }
-
-    private IEnumerator FadeInLineFive()
-    {
-        StartCoroutine(FadeOutLineFour());
-        lineFive_Animator.Play("LineFive_FadeIn");
-        yield break;
-    }
-
-    private IEnumerator FadeOutLineFive()
-    {
-        yield break;
+        StartCoroutine(FadeInLineFive());
     }
     #endregion
 
+    #region SectionTwo
+    private IEnumerator FadeInLineFive()
+    {
+        lineFive_Animator.Play("LineFive_FadeIn");
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FadeInLineSix());
+    }
+
+    private IEnumerator FadeInLineSix()
+    {
+        lineSix_Animator.Play("LineSix_FadeIn");
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FadeInLineSeven());
+    }
+
+    private IEnumerator FadeInLineSeven()
+    {
+        lineSeven_Animator.Play("LineSeven_FadeIn");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(FadeInLineEight());
+    }
+    private IEnumerator FadeInLineEight()
+    {
+        lineEight_Animator.Play("LineEight_FadeIn");
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(BatchFadeOutTwo());
+    }
+
+    private IEnumerator BatchFadeOutTwo()
+    {
+        lineEight_Animator.Play("LineEight_FadeOut");
+        lineSeven_Animator.Play("LineSeven_FadeOut");
+        lineSix_Animator.Play("LineSix_FadeOut");
+        lineFive_Animator.Play("LineFive_FadeOut");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(LineNine());
+    }
+
+    private IEnumerator LineNine()
+    {
+        lineNine_Animator.Play("LineNine_FadeIn");
+        yield return new WaitForSeconds(3f);
+        lineNine_Animator.Play("LineNine_FadeOut");
+        StartCoroutine(LineTen());
+
+    }
+
+    private IEnumerator LineTen()
+    {
+        lineTen_Animator.Play("LineTen_FadeIn");
+        yield return new WaitForSeconds(3f);
+        lineTen_Animator.Play("LineTen_FadeOut");
+        StartCoroutine(LineEleven());
+    }
+
+    private IEnumerator LineEleven()
+    {
+        lineEleven_Animator.Play("LineEleven_FadeIn");
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(LineTwelve());
+    }
+
+    private IEnumerator LineTwelve()
+    {
+        lineTwelve_Animator.Play("LineTwelve_FadeIn");
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(LineThirteen());
+    }
+
+    private IEnumerator LineThirteen()
+    {
+        lineThirteen_Animator.Play("LineThirteen_FadeIn");
+        yield return new WaitForSeconds(3f);
+        lineThirteen_Animator.Play("LineThirteen_FadeOut");
+        lineTwelve_Animator.Play("LineTwelve_FadeOut");
+        lineEleven_Animator.Play("LineEleven_FadeOut");
+        StartCoroutine(LineFourteen());
+    }
+
+    private IEnumerator LineFourteen()
+    {
+        lineFourteen_Animator.Play("LineFourteen_FadeIn");
+        yield return new WaitForSeconds(5f);
+        lineFourteen_Animator.Play("LineFourteen_FadeOut");
+    }
+    #endregion
 }
--- TeaGame/Assets/Ink/TisaneInk/tisaneInk.ink
+++ TeaGame/Assets/Ink/TisaneInk/tisaneInk.ink
@@ -1,19 +1,15 @@
-
 Are you sure this is what you want?
-
 Yes.
-
 I just want to make sure you thought this through.
-
 I have... For the first time I'm sure this is what I want.
-
 You've never even been outside the city.
-
 How bad can it be?
-
-have you even talked to anyone who does something like this?
-
+Have you even talked to anyone who does something like this?
 No...because they're out there.
-
-Not Here
+Not Here...
+Why does it have to be you?
+It doesn't.
+But I want to do something that matters.
+Besides...
+How hard can it be?
     -> END
