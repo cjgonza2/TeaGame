290252453e30ad9d2d0757f0ef27f2b2b2f79839
Tassneen Bashir <tassneen.bashir@gmail.com>
2023-04-23 16:30:18 -0400

    added page flip animation sprites to title menu

--- TeaGame/Assets/TazDev/Scripts/TitleMenuManager.cs
+++ TeaGame/Assets/TazDev/Scripts/TitleMenuManager.cs
@@ -10,34 +10,117 @@ public class TitleMenuManager : MonoBehaviour
     public GameObject playButtonObj;
     public GameObject optionsButtonObj;
     public GameObject creditsButtonObj;
+    public GameObject backButtonObj;
     //public GameObject clickToStart;
 
     public float delayTime;
 
+    //getting Button components
     private Button StartButton()
     {
         return startButtonObj.GetComponent<Button>();
     }
+
+    private Button PlayButton()
+    {
+        return playButtonObj.GetComponent<Button>();
+    }
+
+    private Button OptionsButton()
+    {
+        return optionsButtonObj.GetComponent<Button>();
+    }
+
+    private Button CreditsButton()
+    {
+        return creditsButtonObj.GetComponent<Button>();
+    }
+
+    private Button BackButton()
+    {
+        return backButtonObj.GetComponent<Button>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         
-        //creates button to add listener too. 
+        //add listener to Start Button, click for TaskOnStart
         Button openBook = StartButton();
-        openBook.onClick.AddListener(TaskOnClick); //adds listener for exit button. when clicked, runs TaskOnClick function
+        openBook.onClick.AddListener(TaskOnStart);
+
+        //add listener to Play Button, click for TaskOnPlay
+        Button playGame = PlayButton();
+        playGame.onClick.AddListener(TaskOnPlay);
+
+        //add listener to Options Button, click for TaskOnOptions
+        Button openOptions = OptionsButton();
+        openOptions.onClick.AddListener(TaskOnOptions);
+
+        //add listener to Credits Button, click for TaskOnCredits
+        Button openCredits = CreditsButton();
+        openCredits.onClick.AddListener(TaskOnCredits);
+
+        //add listener to Back Button, click for TaskOnBack
+        Button goBack = BackButton();
+        goBack.onClick.AddListener(TaskOnBack);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void TaskOnClick()
+    //do this when click start button
+    private void TaskOnStart()
     {
-        Debug.Log("clicked");
+        Debug.Log("start");
         startButtonObj.SetActive(false);
         StartCoroutine(StartButtonDelay());
+    }
+
+    //do this when click play button
+    private void TaskOnPlay()
+    {
+        Debug.Log("play");
+    }
+
+    //do this when click options button
+    private void TaskOnOptions()
+    {
+        Debug.Log("options");
+
+        //pop out back button
+        backButtonObj.SetActive(true);
+
+        //get rid of all start menu buttons
+        playButtonObj.SetActive(false);
+        creditsButtonObj.SetActive(false);
+        optionsButtonObj.SetActive(false);
+    }
+
+    //do this when click credits button
+    private void TaskOnCredits()
+    {
+        Debug.Log("credits");
+
+        //pop out back button
+        backButtonObj.SetActive(true);
+
+        //get rid of all start menu buttons
+        playButtonObj.SetActive(false);
+        creditsButtonObj.SetActive(false);
+        optionsButtonObj.SetActive(false);
+
+    }
+
+    //do this when click back button
+    private void TaskOnBack()
+    {
+        Debug.Log("back");
+        backButtonObj.SetActive(false);
+
+        //bring back all start menu buttons
+        playButtonObj.SetActive(true);
+        creditsButtonObj.SetActive(true);
+        optionsButtonObj.SetActive(true);
     }
 
     IEnumerator StartButtonDelay()
